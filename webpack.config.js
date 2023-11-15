6db528b2f4ab2b7be5776a57dbeb1ed5c86a6ddb
udin-imad <udinwebdev@gmail.com>
2023-11-15 18:33:29 +0700

    initialize dependency and configuration

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: './js/main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+    compress: true,
+    port: 9000,
+    liveReload: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: './src/index.html'
+      },
+    ),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
