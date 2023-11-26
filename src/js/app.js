9e53869384078e686f10a6a58358f63d628b717c
udin-imad <udinwebdev@gmail.com>
2023-11-26 09:45:38 +0700

    delete unnecessery variable

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,11 @@ import { compareAsc, startOfWeek, endOfWeek, isWithinInterval } from "date-fns";
 export function app() {
   const taskList = [];
   class Task {
-    constructor(title, description, date, priority, status, tag) {
+    constructor(title, description, date, priority, tag) {
       this.title = title
       this.description = description
       this.dueDate = this.getDueDate(date)
       this.priority = priority
-      this.status = this.getStatus(status)
       this.tag = tag
     }
     getStatus(status) {
@@ -24,18 +23,17 @@ export function app() {
     taskList.push(task)
   }
 
-  function createNewTask(title, description, date, priority, status, tag) {
-    const task = new Task(title, description, date, priority, status, tag)
+  function createNewTask(title, description, date, priority, tag) {
+    const task = new Task(title, description, date, priority, tag)
 
     addTaskToList(task)
   }
 
-  function editTask(index, title, description, date, priority, status, tag) {
+  function editTask(index, title, description, date, priority, tag) {
     taskList[index].title = title
     taskList[index].description = description
     taskList[index].dueDate = date
     taskList[index].priority = priority
-    taskList[index].status = taskList[index].getStatus(status)
     taskList[index].tag = tag
   }
 
